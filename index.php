3e71e1bb880990c52eab1de95878da49d6ff08ca
LeaStclbt <lea.steclebout@gmail.com>
2022-02-25 09:55:22 +0100

    page prestation

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
                 <path id="Tracé_676" data-name="Tracé 676" d="M4750.45,1200.881l14.042-14.544,14.544,14.544,14.544-14.544,14.544,14.544,14.544-14.544,14.544,14.544,14.543-14.544" transform="translate(-4749.371 -1184.198)" fill="none" stroke="#fff" stroke-width="3"/>
             </svg>
             <h1> Créaoow </h1>
-            <h2> Une agence web à votre image !</h2>
+            <h2>Une agence web à votre image !</h2>
             <a href=''> Réalisez votre projet </a>
         </div>
         <img src='images/illustration_home.png'>
@@ -23,9 +23,9 @@
             <path id="Tracé_671" data-name="Tracé 671" d="M4750.45,1200.881l14.042-14.544,14.544,14.544,14.544-14.544,14.544,14.544,14.544-14.544,14.544,14.544,14.543-14.544" transform="translate(-4749.371 -1184.198)" fill="none" stroke="#eb5a53" stroke-width="3"/>
         </svg>
     </div>
-    <div>
+    <div class='presentation-texte'>
         <p>
-            Créaoow, est     une agence de communication spécialisée en design et de développement web.Nos designers issus de formations effervescentes, de parcours professionnels divergents et d’une addiction commune pour l’image, vous accompagnent dans la création de stratégies, de design et de développement web.
+            Créaoow, est une agence de communication spécialisée en design et de développement web. Nos designers issus de formations effervescentes, de parcours professionnels divergents et d’une addiction commune pour l’image, vous accompagnent dans la création de stratégies, de design et de développement web.
         </p>
         <p>
             Indications : L'agence Créaoow est recommandée pour tout projet de création de logo, d’identité visuelle, de charte graphique et de site web.
@@ -33,7 +33,37 @@
         <p> Posologie : Dès l’apparition des symptômes, nous contacter immédiatement... </p>
     </div>
     <div>
+    </div>
+</section>
 
+<section class='valeurs'>
+    <div>
+        <img src='images/picto-disponibilité.png'>
+        <h3> Disponibilité </h3>
+        <p> 
+            La qualité de nos services dépend également de notre disponibilité. A la moindre question, il vous suffit de nous contacter par mail ou par téléphone pour obtenir des réponses !
+        </p>
+    </div>
+    <div>
+        <img src='images/picto-passion.png'>
+        <h3> Passion </h3>
+        <p>
+            Notre équipe est passionnée par le monde du numérique et du design graphique. Nous mettons en commun nos connaissances et nos capacités pour vous offrir un projet qui vous ressemble, annonçant un nouveau chapitre pour votre entreprise.
+        </p>
+    </div>
+    <div>
+        <img src='images/picto-confiance.png'>
+        <h3> Confiance </h3>
+        <p>
+            Partenaires de vos projets, nous établissons une relation de confiance pour vous accompagner dans les réflexions, vous aider à prendre les bonnes décisions et avancer ensemble.
+        </p>
+    </div>
+    <div>
+        <img src='images/picto-collaboration.png'>
+        <h3> Collaboration </h3>
+        <p>
+        Nous réalisons votre projet en étant à l’écoute de vos envies pour qu’il soit unique et authentique. Mais pas de panique ! Nous vous conseillons et vous aidons avec sagesse dans vos choix pour qu’ils correspondent aux tendances graphiques et numériques actuelles.
+        </p>
     </div>
 </section>
 
--- prestations.php
+++ prestations.php
@@ -2,7 +2,138 @@
     include 'partials/header.html';
 ?>
 
+<section class="prestaBanner">
+    <div class='prestaContainer prestaBanner-elements'>
+    <img src='images/illustration_presentation.png'>
+        <div>
+            <h2> Une approche qui veut que le design soit la somme de l'intention de transformation (le dessein) et de l'imagination qui prend 
+                forme (le dessin). En matière d'identité visuelle, cela se traduit par l'idée qu'un logotype doit autant "décrire le donné" 
+                (analyse) que de "pointer le visé" (projection). 
+            </br> </br>
+                Notre volonté est de raconter le maximum de choses en un minimum d'espace. Il s'agit aussi d'œuvrer à éliminer la 
+                complexité au profit de l'essentiel, de penser durable et de viser l'indémodable. 
+             </br> </br>
+                Nous abordons la création de design de manière adéquate avec votre projet. 
+            </h2>
+            </br>
+            <svg class="prestaSVG" xmlns="http://www.w3.org/2000/svg" width="103.445" height="18.805" viewBox="0 0 103.445 18.805">
+                <path id="Tracé_676" data-name="Tracé 676" d="M4750.45,1200.881l14.042-14.544,14.544,14.544,14.544-14.544,14.544,14.544,14.544-14.544,14.544,14.544,14.543-14.544" transform="translate(-4749.371 -1184.198)" fill="none" stroke="#fff" stroke-width="3"/>
+            </svg>
+        </div>
+        
+    </div>
+</section>
 
+<section class='prestation'>
+
+        <h3 class='titrePresta'> Nos Prestations </h3>
+
+    <div class='prestaText'>
+
+        <div> 
+            <img class="prestaImg" src='images/picto_webDesign.png'>
+            <h3>   Web Design   </h3>
+            <p> 
+                Nos compétences en Web Design nous permettent de concevoir votre interface web et de vous proposer des maquettes pour le design de votre site.
+            </p>
+        </div>
+
+        <div>
+            <img class="prestaImg" src='images/picto_balise.png'>
+            <h3> Développement </h3>
+            <p> 
+                Nous créons et programmons votre site web en fonction de vos besoins.
+            </p>
+        </div>
+
+        <div>
+            <img class="prestaImg" src='images/picto_empreinte.png'>
+            <h3> Identité visuelle </h3>
+            <p> 
+                Nous développons votre identié visuelle grâce à des chartes graphiques et des éléments visuels tels que des logos.
+            </p>
+        </div>
+
+        <div>
+            <img class="prestaImg" src='images/picto_play.png'>
+            <h3> Motion design </h3>
+            <p> 
+                Grâce au motion design nous pouvons donner vie à des éléments graphiques.
+            </p>
+        </div>
+
+        <div>
+            <img class="prestaImg" src='images/picto_graph.png'>
+            <h3> Référencement </h3>
+            <p> 
+                Nous gérons le positionnement de votre site sur les moteurs de recherche en réponse aux demandes des internautes.
+            </p>
+        </div>
+
+    </div>
+
+</section>
+
+
+<section class="methodologie">
+   <h3 class='titreMetho'> Méthodologie </h3>
+
+   <div class="methoText">
+
+        <div>
+            <p>01</p>
+            <h3>Premier contact</h3>
+            <p>Contact
+                <br>Premier rendez-vous
+                <br>Briefing avec le client
+            </p>
+        </div>
+
+        <div>
+            <p>02</p>
+            <h3>Étude du projet</h3>
+            <p>Analyse des besoins
+                <br>Concertation
+                <br>Idées
+            </p>
+        </div>
+
+        <div class="methoUpper">
+            <p>03</p>
+            <h3>Design de marque</h3>
+            <p>Création de logo
+                <br>Création d'identités visuelles
+                <br>Charte graphique
+            </p>
+        </div>
+
+        <div class="methoUpper uppertwo">
+            <p>04</p>
+            <h3>Production outils</h3>
+            <p>Webdesign / Maquette
+                <br>Print communication
+                <br>Référencement
+                <br>Rédaction de contenu 
+                <br>Création de site web
+                <br>Motion design et vidéo
+                <br>Réseaux sociaux
+            </p>
+        </div>
+
+   </div>
+
+</section>
+
+<section class="prestaContact">
+
+    <img class="contactLogo" src="images/yeux_hibou.png">
+    <div class="contactContainer">
+        <img class="contactImg" src='images/illu_presentation.png'>
+        <h3>Pour toutes informations supplémentaires ou pour prendre rendez-vous, n'hésitez pas à nous contacter !</h3>
+        <button>Contacter-nous !  <img class="contactButtonImg"src="images/yeux_hibou.png"></button>
+    </div>
+
+</section>
 
 <?php
     include 'partials/footer.html';
